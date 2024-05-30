c30f6f7a606cc0642afa039e699a9392f0d51d0e
adrtivv <adrtivv@gmail.com>
2024-05-31 02:46:17 +0530

    TT

--- init.sh
+++ init.sh
@@ -1,40 +1,37 @@
-#!/bin/bash
-
-set -e
-current_script_dir=$(dirname "$(realpath "$0")")
-# make sure ansible is installed
-command -v ansible > /dev/null 2>&1
+# set -e
+CURRENT_SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# GITHUB_USERNAME=adrtivv
 # install ansible if not found on the $PATH
-if [ $? -ne 0 ];then
-  sudo dnf install -y ansible
-fi
+# if ! command -v ansible > /dev/null; then
+#   sudo dnf install -y ansible
+# fi
 
-while true; do
-  echo "select an option:- "
-  echo "1. backup"
-  echo "2. restore"
-  echo "3. exit"
-  read option
-  case $option in
-    1)
-      ansible-playbook -K "$current_script_dir/backup/main.yaml"
-      chezmoi git commit -am "TT"
-      chezmoi git push origin main
-      break
-    ;;
-    2)
-      ansible-playbook -K "$current_script_dir/restore/main.yaml"
-      # setup the source directory, generate the config file, and update 
-      # the destination directory to match the target state.
-      chezmoi init --apply https://github.com/$GITHUB_USERNAME/dotfiles.git
-      break
-    ;;
-    3)
-      exit 0
-    ;;
-    *)
-      echo "invalid option selected, try again"
-      continue
-    ;;
-  esac
-done
+# while true; do
+#   echo "select an option:- "
+#   echo "1. backup"
+#   echo "2. restore"
+#   echo "3. exit"
+#   read option
+#   case $option in
+#     1)
+      # ansible-playbook -K "$CURRENT_SCRIPT_DIR/backup/main.yaml"
+#       chezmoi git commit -am "TT"
+#       chezmoi git push origin main
+#       break
+#     ;;
+#     2)
+      ansible-playbook -K "$CURRENT_SCRIPT_DIR/restore/main.yaml"
+#       # setup the source directory, generate the config file, and update 
+#       # the destination directory to match the target state.
+#       chezmoi init --apply https://github.com/$GITHUB_USERNAME/dotfiles.git
+#       break
+#     ;;
+#     3)
+#       exit 0
+#     ;;
+#     *)
+#       echo "invalid option selected, try again"
+#       continue
+#     ;;
+#   esac
+# done
